27636a8dd209a730e141147de8b61d8408138eec
DestaWork21 <workyalew21@gmail.com>
2018-01-15 12:58:54 -0500

    first commit

---
+++ src/main/java/com/desta/controllersample/ControllersampleApplication.java
@@ -0,0 +1,15 @@
+package com.desta.controllersample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class ControllersampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllersampleApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/desta/controllersample/ControllersampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.desta.controllersample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControllersampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
